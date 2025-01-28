7b642706d7f8ef18350b7544b23afb513ac9d4a4
Ngure Kibaara <ngurekibaara01@gmail.com>
2025-01-28 13:54:54 +0300

    Secure Store

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -13,6 +13,7 @@ import AboutScreen from "./AboutScreen";
 import ContactScreen from "./ContactScreen";
 import ReservationScreen from "./ReservationScreen";
 import FavoritesScreen from "./FavoritesScreen";
+import LoginScreen from "./LoginScreen";
 import { Icon } from "react-native-elements";
 import logo from "../assets/images/logo.png";
 import { useDispatch } from "react-redux";
@@ -147,6 +148,29 @@ const FavoritesNavigator = () => {
   );
 };
 
+const LoginNavigator = () => {
+  const Stack = createStackNavigator();
+
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={({ navigation }) => ({
+          headerLeft: () => (
+            <Icon
+              name="sign-in"
+              type="font-awesome"
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const DirectoryNavigator = () => {
   const Stack = createStackNavigator();
   return (
@@ -216,8 +240,25 @@ const Main = () => {
           headerShown: true,
         }}
       >
+
+      <Drawer.Screen
+          name="Login"
+          component={LoginNavigator}
+          options={{
+            headerShown: false,
+            drawerIcon: ({ color }) => (
+              <Icon
+                name="sign-in"
+                type="font-awesome"
+                size={24}
+                iconStyle={{ width: 24 }}
+                color={color}
+              />
+            ),
+          }}
+        />
         <Drawer.Screen
-          name="HomeNav"
+          name="Home"
           component={HomeNavigator}
           options={{
             title: "Home",
